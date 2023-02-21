c5842bedadf169596de22093876fd8252f19d6be
Guillermo Preisser <guillermopreisser@hotmail.com>
2023-02-21 15:15:22 +0100

    Update s3mps_v3.jl

--- src/s3mps_v3.jl
+++ src/s3mps_v3.jl
@@ -1,1 +1,29 @@
+using LinearAlgebra
+using ITensors
 
+import LinearAlgebra.norm
+import LinearAlgebra.tr
+
+####################################################################################################
+#
+# Finite sized MPS structure
+#
+####################################################################################################
+
+
+mutable struct s3mps
+
+    Na::Int      # number of sites
+    Nc::Int     # max number of photons mode cavity
+    chi::Int    # max bond dimension allowed
+
+    ocen::Int               # site which is the orthonormality center
+    gams::Vector{ITensor}   # gam 3-tensors
+
+    is::Vector{Index}       # ITensor physical indices.
+    chis::Vector{Index}    # ITensor bond indices (1 is leftmost)
+
+    # constructor
+    s3mps(Nc::Int,Na::Int, chi::Int) = new(Nc, Na, chi, 1, Vector{ITensor}(), Vector{Index}(), Vector{Index}())
+
+end
